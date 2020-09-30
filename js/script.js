39ad1e05c343216b0d2c042370dd5a3242c5ce90
MaperPario <69398573+MaperPario@users.noreply.github.com>
2020-09-29 21:44:28 -0700

    updated to use es6

--- js/script.js
+++ js/script.js
@@ -3,31 +3,49 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
- * `quotes` array 
+ * Created an array called 'quotes' and stored 5 famous quotes in the individual objects.
 ***/
-
+let quotes = [
+  {quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+  source: 'Nelson Mandela',},
+  {quote: 'If at first you don\'t succeed, try, try again.', source: 'William Edward Hickson',},
+  {quote: 'The object of life is not to be on the side of the majority, but to escape finding oneself in the ranks of the insane.', 
+  source: 'Marcus Aurelius'},
+  {quote: 'Life is what happens when you\'re making other plans.', source: 'John Lennon', citation: 'Song: "Beautiful Boy(Darling Boy)"'},
+  {quote: 'Stop breaking your own heart pretending people are something they\'re not.', source: 'Instagram: @werenotreallystrangers', year: '2020'}
+];
 
 
 /***
- * `getRandomQuote` function
+ *  Created a 'getRandomQuote' function that returns a random quote from the array 'quotes'.
 ***/
-
+function getRandomQuote() {
+  randomQuote = Math.floor(Math.random() * quotes.length);
+  return(quotes[randomQuote]);
+}
 
 
 /***
- * `printQuote` function
+ * Created a function that displays the random quote and source, along with a citation and year if it contains either as a property.
 ***/
+function printQuote() {
+  const quoteToPrint = getRandomQuote();
+  let html = `
+  <p class="quote">${quoteToPrint.quote}</p>
+  <p class="source">${quoteToPrint.source}
+  `;
+  
+  if (quoteToPrint.citation) {
+    html += `<span class="citation">${quoteToPrint.citation}</span>`;  
+  } 
+  
+  if (quoteToPrint.year) {
+    html += `<span class="year">${quoteToPrint.year}</span>`;  
+  }
+  html += '</p>';
 
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+  document.getElementById('quote-box').innerHTML = html;
+}
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
