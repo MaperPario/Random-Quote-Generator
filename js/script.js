27f4431b03f093b5b46ce90786f4bbcc5643d7af
MaperPario <69398573+MaperPario@users.noreply.github.com>
2020-10-03 17:46:53 -0700

    Updated for extra credit!

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ project 1 - A Random Quote Generator
 ***/
 let quotes = [
   {quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-  source: 'Nelson Mandela',},
-  {quote: 'If at first you don\'t succeed, try, try again.', source: 'William Edward Hickson',},
+  source: 'Nelson Mandela', tags: ['historic', 'leadership', 'idolism']},
+  {quote: 'If at first you don\'t succeed, try, try again.', source: 'William Edward Hickson'},
   {quote: 'The object of life is not to be on the side of the majority, but to escape finding oneself in the ranks of the insane.', 
-  source: 'Marcus Aurelius'},
+  source: 'Marcus Aurelius', tags: ['philosophy', 'stoicism', 'mindfulness']},
   {quote: 'Life is what happens when you\'re making other plans.', source: 'John Lennon', citation: 'Song: "Beautiful Boy(Darling Boy)"'},
   {quote: 'Stop breaking your own heart pretending people are something they\'re not.', source: 'Instagram: @werenotreallystrangers', year: '2020'}
 ];
@@ -27,9 +27,11 @@ function getRandomQuote() {
 
 
 /***
- * Created a function that displays the random quote and source, along with a citation and year if it contains either as a property.
+ * Created a function that displays the random quote and source, along with a citation, year and tags if it contains any as a property.
 ***/
 function printQuote() {
+  changeBackgroundColor();
+
   const quoteToPrint = getRandomQuote();
   let html = `
   <p class="quote">${quoteToPrint.quote}</p>
@@ -43,9 +45,37 @@ function printQuote() {
   if (quoteToPrint.year) {
     html += `<span class="year">${quoteToPrint.year}</span>`;  
   }
+
+  if (quoteToPrint.tags) {
+    html += `<p class="tags">${quoteToPrint.tags.join(', ')}</p>`;  
+  }
+
   html += '</p>';
 
   document.getElementById('quote-box').innerHTML = html;
 }
+
+/***
+ * Created a function that changes the background color anytime a new quote is generated from printQuote().
+***/
+function changeBackgroundColor() {
+  const red = getRandomColorValue();
+  const green = getRandomColorValue();
+  const blue = getRandomColorValue();
+  document.body.style.background = (`rgb(${red}, ${green}, ${blue})`);
+}
+
+/***
+ * Created a function that obtains a random RGB value. The changeBackgroundColor function uses this.
+***/
+function getRandomColorValue() {
+  let randomColor = Math.floor(Math.random() * 256);
+  return randomColor;
+}
+
+/***
+ * Used the setInterval method to call the printQuote function every ten seconds, or 10000 milliseconds.
+***/
+setInterval(printQuote, 10000);
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
